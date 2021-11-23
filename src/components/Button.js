51eb47133c18997a4952e141aca59574101f6835
Casper Yonel <yonelcasper@gmail.com>
2021-11-23 15:34:48 -0700

    working except new thank you

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,32 +4,36 @@ import React, {useState} from "react";
     // taret is teh username, using setusername as the function to creeate and add the new state, 
 
 const Button = () => {
-    const [email, setEmail] = useState({
+    let [email, setEmail] = useState({
         email: ''
     })
 
     const submit = e => {
 
+
         e.preventDefault()
 
-        // let email = e.target.value 
-        // this.state = {
-        //     email: ''
-        // }
+        if (!e.target.value === '') {
+            alert('Please add an email')
+            return
+        }
+        // this doesn't work currently
 
         axios.post('/waitlist', { email })
         .then(res => console.log(res))
         .catch(err => console.log(err))
     
+        setEmail('')
+
     }
     
     return (
         <form >
             <label>
                 Email: 
-                 <input placeholder="email" type="text" onfocus="this.value=''" onChange={e => setEmail(e.target.value)}/>
+                 <input placeholder="email" type="text" onChange={e => setEmail(e.target.value)}/>
             </label>
-            <button onClick={submit} type="submit" className='btn'>Join the Waitlist</button>
+            <button onClick={submit} className='btn'>Join the Waitlist</button>
         </form>
     )
 }
