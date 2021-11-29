2955cc0b9bd88d3cee74dfc4feeeb765a3ac59f0
Casper Yonel <yonelcasper@gmail.com>
2021-11-29 13:38:22 -0700

    updated typerwriter

--- src/components/Waitlist.js
+++ src/components/Waitlist.js
@@ -4,8 +4,27 @@ const Waitlist = ({ title }) => {
     return (
             <div className='waitlist-container'>
                 <div className='waitlist-container-inner'>
-                    <h3>Join us on our journey.</h3>
+                <div className='wailist-container-inner-animation'>
+                    <div className='wrapper'> 
+                        <div className='static-txt'>Pay upfront salary to your employees every </div>
+                        <ul className='dynamic-txts'>
+                            <li><span>month</span></li>
+                            <li><span>quarter</span></li>
+                            <li><span>week</span></li>
+                        </ul>
+                    </div>
+                </div>
+                    {/* <span >Pay your employees upfront every </span>
+                    <span className="main-header">quarter.</span> */}
+                    
+                    
+                    
+                    
+                    
+                    
+                    
                     {/* <input placeholder={title} type="text" /> */}
+                    <h3>We’re a capital generative marketplace that enables companies to provide upfront salary for their employees at zero cost.</h3>
                     <Button />
                 </div>
             </div>
